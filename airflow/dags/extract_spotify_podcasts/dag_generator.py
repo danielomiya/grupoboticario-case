bdc0bb864a2c2c0cbffe1d3d9f0db59282b25518
Daniel Omiya <danielomiya@gmail.com>
2023-03-14 19:40:52 -0300

    add dag to extract a spotify search

---
+++ airflow/dags/extract_spotify_podcasts/dag_generator.py
@@ -0,0 +1,73 @@
+"""
+### extract_spotify_podcasts
+
+The present DAG extracts a Spotify search, transforms it and loads it into
+Google BigQuery.
+"""
+
+import os
+import typing as t
+
+from airflow.decorators import dag, task
+from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
+from airflow.utils import dates
+from shared.spotify_client import SpotifyClient
+
+if t.TYPE_CHECKING:
+    from pandas import DataFrame
+
+
+@task(multiple_outputs=False)
+def extract_podcasts() -> "DataFrame":
+    import pandas as pd
+
+    client = SpotifyClient(
+        os.getenv("SPOTIFY_CLIENT_ID"),
+        os.getenv("SPOTIFY_CLIENT_SECRET"),
+    )
+    items = client.search("data hackers", type="episode")
+    return pd.json_normalize(items, sep="__")
+
+
+@task(multiple_outputs=False)
+def transform_data(df: "DataFrame") -> "DataFrame":
+    return df.drop(["images", "languages"], axis=1)
+
+
+@task(multiple_outputs=False)
+def load_into_gbq(
+    df: "DataFrame",
+    gcp_conn_id: str = "google_cloud_default",
+) -> None:
+    hook = GoogleBaseHook(gcp_conn_id)
+    credentials, project_id = hook.get_credentials_and_project_id()
+
+    df.to_gbq(
+        "staging.data_hackers_podcasts",
+        if_exists="replace",
+        project_id=project_id,
+        credentials=credentials,
+    )
+
+
+default_args = {
+    "owner": "airflow",
+    "gcp_conn_id": "google_cloud_default",
+}
+
+
+@dag(
+    start_date=dates.days_ago(1),
+    schedule="@once",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    doc_md=__doc__,
+)
+def extract_spotify_podcasts():
+    raw_df = extract_podcasts()
+    final_df = transform_data(raw_df)
+    load_into_gbq(final_df)
+
+
+extract_spotify_podcasts()
---
+++ airflow/dags/shared/spotify_client.py
@@ -0,0 +1,101 @@
+import time
+import typing as t
+
+import requests
+
+
+def pluralize(name: str) -> str:
+    # silly method to make code more readable
+    return name + "s"
+
+
+class SpotifyAuth(requests.auth.AuthBase):
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        session: requests.Session = None,
+    ) -> None:
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token: str = None
+        self.expires_at: int = None
+
+        if session:
+            self.session = session
+        else:
+            self.session = requests.Session()
+
+    def __call__(self, r: requests.Request) -> requests.Request:
+        if not r.url.startswith("https://api.spotify.com"):
+            return r
+
+        if self.is_current_token_expired:
+            token_data = self.request_access_token()
+            self.store_token(token_data)
+
+        r.headers["Authorization"] = f"Bearer {self.access_token}"
+        return r
+
+    def request_access_token(self):
+        response = self.session.post(
+            "https://accounts.spotify.com/api/token",
+            data={
+                "grant_type": "client_credentials",
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+            },
+        )
+        response.raise_for_status()
+        print(response.text)
+        return response.json()
+
+    def store_token(self, token_data: t.Dict[str, t.Any]) -> None:
+        self.expires_at = int(time.time()) + token_data["expires_in"]
+        self.access_token = token_data["access_token"]
+
+    @property
+    def is_current_token_expired(self) -> bool:
+        if self.expires_at is None:
+            return True
+
+        now = int(time.time())
+        return now >= self.expires_at
+
+
+class SpotifyClient:
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        self.session = requests.Session()
+        self.session.auth = SpotifyAuth(
+            client_id, client_secret, session=self.session
+        )
+
+    def search(
+        self,
+        query: str,
+        type: str,
+        market: str = "BR",
+        limit: int = 50,
+        offset: int = 0,
+    ) -> t.Dict[str, t.Any]:
+        response = self.session.get(
+            "https://api.spotify.com/v1/search",
+            headers={
+                "Accept": "application/json",
+            },
+            params={
+                "q": query,
+                "market": market,
+                "type": type,
+                "limit": limit,
+                "offset": offset,
+            },
+        )
+        response.raise_for_status()
+        json = response.json()
+
+        if "," not in type:
+            return json[pluralize(type)]["items"]
+        else:
+            types = type.split(",")
+            return {t: json[pluralize(t)]["items"] for t in types}
