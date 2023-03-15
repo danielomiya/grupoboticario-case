12894bbab807f58ca792fd84cf49869b3af89a66
Daniel Omiya <danielomiya@gmail.com>
2023-03-15 04:16:20 -0300

    add def to parse yaml either from path or buffer

--- airflow/dags/shared/yaml.py
+++ airflow/dags/shared/yaml.py
@@ -1,6 +1,8 @@
 import os
 import re
+import typing as t
 from functools import reduce
+from pathlib import Path
 
 import yaml
 
@@ -25,4 +27,18 @@ yaml.add_implicit_resolver("!envvar", ENV_VAR_PATTERN, Loader=yaml.SafeLoader)
 yaml.add_constructor("!envvar", env_var_constructor, Loader=yaml.SafeLoader)
 
 
-safe_load = yaml.safe_load
+def safe_load(
+    path_or_buf: t.Union[str, os.PathLike, t.IO[str], t.IO[bytes]]
+) -> t.Any:
+    if isinstance(path_or_buf, os.PathLike):
+        path = path_or_buf.__fspath__()
+    elif isinstance(path_or_buf, str) and Path(path_or_buf).is_file():
+        path = path_or_buf
+    else:
+        path = None
+
+    if path:
+        with open(path) as f:
+            return yaml.safe_load(f)
+
+    return yaml.safe_load(path_or_buf)
